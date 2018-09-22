41f943d08653bd7c1bd0399ab1029f19ab7b8f19
Maher Soua <mahersoua@gmail.com>
2018-09-22 14:12:18 +0200

    Remove useless files

--- app/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
+++ app/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
@@ -57,7 +57,6 @@ public class ArticleDetailFragment extends Fragment implements
     private long mItemId;
     private View mRootView;
     private int mMutedColor = 0xFF333333;
-    private ConstraintLayout mDrawInsetsFrameLayout;
     private ColorDrawable mStatusBarColorDrawable;
 
     private int mTopInset;
@@ -188,7 +187,7 @@ public class ArticleDetailFragment extends Fragment implements
         TextView titleView = mRootView.findViewById(R.id.article_title);
         TextView bylineView = mRootView.findViewById(R.id.article_byline);
         bylineView.setMovementMethod(new LinkMovementMethod());
-        TextView bodyView = (TextView) mRootView.findViewById(R.id.article_body);
+        TextView bodyView = mRootView.findViewById(R.id.article_body);
 
 
         bodyView.setTypeface(Typeface.createFromAsset(getResources().getAssets(), "Rosario-Regular.ttf"));
